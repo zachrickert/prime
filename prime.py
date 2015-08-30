0bb25d00f9d0d3cb47d71fbf4fa79dd5c74d87aa
Zach Rickert <zachrickert@gmail.com>
2015-08-29 20:49:51 -0700

    initial commit

---
+++ prime.py
@@ -0,0 +1,19 @@
+#Prime
+#This program will print out all the prime numbers up to a given number.
+
+def main():
+  print("This program will find all the primes up to a given number.")
+
+  x = input("Enter a number: ")
+  topNumber = x+1
+  isPrime = [True]*topNumber
+
+  for i in range(2,topNumber):
+    if isPrime[i]:
+      for j in range(i,topNumber,i):
+        isPrime[j] = False
+
+    print i
+
+main()
+
