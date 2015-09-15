fdfc796c62a10f30dcf2b702487ef64fa035995d
Zach Rickert <zachrickert@gmail.com>
2015-09-15 13:10:02 -0700

    Add ablity to directly input a number at command line.

--- prime.py
+++ prime.py
@@ -1,27 +1,33 @@
-#Prime
-#This program will print out all the prime numbers up to a given number.
+# Prime
+# This program will print out all the prime numbers up to a given number.
+
 
 def main():
-  print("This program will find all the primes up to a given number.")
+    import sys
 
-  x = input("Enter a number: ")
-  topNumber = int(x)+1
-  isPrime = [True]*topNumber
-  count = 0
+    print("This program will find all the primes up to a given number.")
 
-  for i in range(2,topNumber):
-    if isPrime[i]:
-      for j in range(i**2,topNumber,i):
-        isPrime[j] = False
+    if (len(sys.argv) == 2):
+        x = sys.argv[1]
+    else:
+        x = input("Enter a number: ")
 
-      count = count +1
-      if count == 10:
-        print (i)
-        count = 0
-      else:
-        print (str(i) + "\t", end="")
+    topNumber = int(x) + 1
+    isPrime = [True] * topNumber
+    count = 0
 
-  print()
+    for i in range(2, topNumber):
+        if isPrime[i]:
+            for j in range(i ** 2, topNumber, i):
+                isPrime[j] = False
+
+            count = count + 1
+            if count == 10:
+                print (i)
+                count = 0
+            else:
+                print (str(i) + "\t", end="")
+
+    print()
 
 main()
-
